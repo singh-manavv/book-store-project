933633b579ce10f1916de1f272df62c909222c67
KiritSuthar <99836482+KiritSuthar@users.noreply.github.com>
2022-03-01 01:18:53 +0530

    contact page
    contact page has been done.

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,15 @@
+from email import message
 from django.db import models
 
+
 # Create your models here.
-class contect(models.model):
-    name=models.CharField(max_length=100)
-    email=models.CharField(max_length=100)
-    subject=models.CharField()
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    remarks = models.TextField()
+
+    def __str__(self):
+        return self.name
--- myapp/views.py
+++ myapp/views.py
@@ -1,20 +1,41 @@
+import email
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
+
+
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
 
 def contact(request):
-    return render(request,'contact.html')
+    if request.method=="POST":
+        Contact.objects.create(
+            name=request.POST['name'],
+            email=request.POST['email'],
+            subject=request.POST['subject'],
+            remarks=request.POST['remarks']
+        )
+
+    
+        return render(request, 'contact.html')
+
+    else:
+        return render(request, 'contact.html')
+
 
 def about(request):
-    return render(request,'about.html')
-    
+    return render(request, 'about.html')
+
+
 def signup(request):
-    return render(request,'signup.html')
+    return render(request, 'signup.html')
+
 
 def signin(request):
-    return render(request,'signin.html')
+    return render(request, 'signin.html')
+
 
 def categories(request):
-    return render(request,'categories.html')
+    return render(request, 'categories.html')
