15de6862d15c5ac4a14bbf890ab54db2c8c4211f
singh-manvendra <manvendras2608@gmail.com>
2022-03-10 22:13:21 +0530

    Squashed commit of the following:
    commit 285fe17e3faffa004a2f171986f6f97848cdbf49
    Author: KiritSuthar <99836482+KiritSuthar@users.noreply.github.com>
    Date:   Fri Mar 4 19:33:15 2022 +0530
    
        .
    
    commit 17afd3c0f55b1415776b06837ef4b5f8978aa029
    Merge: 4658e49 cd0923d
    Author: KiritSuthar <99836482+KiritSuthar@users.noreply.github.com>
    Date:   Fri Mar 4 19:26:54 2022 +0530
    
        .
    
    commit 4658e497a7d6fa3c456e5a2969105941d4cb0b14
    Author: KiritSuthar <99836482+KiritSuthar@users.noreply.github.com>
    Date:   Fri Mar 4 19:23:17 2022 +0530
    
        seller side
    
        creating html  page from seller side
    
    commit cd0923d998c4d97a1477706563f72fde971e5673
    Author: vanecha-sahil <sahilvanecha3@gmail.com>
    Date:   Fri Mar 4 03:18:24 2022 -0500
    
        New Login
    
        New file
    
    commit 51cf5990ad4dcd91ceaa6da1e7a97ac2e9d14e4a
    Author: vanecha-sahil <sahilvanecha3@gmail.com>
    Date:   Thu Mar 3 08:51:51 2022 -0500
    
        login
    
        new
    
    commit 281ba2030daf29a29afbd86481e1a4623cfdfc11
    Author: vanecha-sahil <sahilvanecha3@gmail.com>
    Date:   Wed Mar 2 23:26:04 2022 -0500
    
        signin_page
    
        create models of signinpage
    
    commit e15ac7706367a8fe65889345361ea8b2a7c442e3
    Author: vanecha-sahil <sahilvanecha3@gmail.com>
    Date:   Tue Mar 1 13:13:37 2022 +0530
    
        signup
    
        sign up page has been done
    
    commit 933633b579ce10f1916de1f272df62c909222c67
    Author: KiritSuthar <99836482+KiritSuthar@users.noreply.github.com>
    Date:   Tue Mar 1 01:18:53 2022 +0530
    
        contact page
    
        contact page has been done.
    
    commit 6e860e030b86035f7e4b573c87c70de3b16eb516
    Author: vanecha-sahil <sahilvanecha3@gmail.com>
    Date:   Sat Feb 26 12:12:32 2022 +0530
    
        models
    
    commit c9c259eb486fcb7b3c16fd4413d5a5a6148f0540
    Author: KiritSuthar <99836482+KiritSuthar@users.noreply.github.com>
    Date:   Fri Feb 25 18:49:11 2022 +0530
    
        login and sign up page
    
        There are i created a login and signup page and both them are linked to header page
    
    commit 03d070eeb4a26edb815c659ece27a4773edc5947
    Author: KiritSuthar <99836482+KiritSuthar@users.noreply.github.com>
    Date:   Mon Feb 21 12:13:08 2022 +0530
    
        some change
    
    commit 94706d7d0a77a59c7929defa31691dff79e2c3d5
    Author: KiritSuthar <99836482+KiritSuthar@users.noreply.github.com>
    Date:   Mon Feb 21 11:43:59 2022 +0530
    
        some changes in header , index and contact pages

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('contact/',views.contact,name='contact'),
     path('about/',views.about,name='about'),
+    path('signup/',views.signup,name='signup'),
+    path('signin/',views.signin,name='signin'),
+    path('categories/',views.categories,name='categories'),
+    path('signout/',views.signout,name='signout'),
+    path('upload_book/',views.upload_book,name='upload_book'),
 ]
